bac0d3879747112afba3ce86f7c3459f8f0e4d74
Snowb <ammoset@gmail.com>
2023-12-29 14:41:06 -0500

    create PoolComponent component, start testing and remember how to Vue

--- src/components/PoolComponent.vue
+++ src/components/PoolComponent.vue
@@ -1,11 +1,28 @@
 <script setup>
+  import { reactive, defineProps } from 'vue';
+  import { Pool } from '../libs/pool-class-v1';
+
+  const props=defineProps({
+    pool: Object,
+  });
+
+  const emit=defineEmits(['addDice']);
+
+  const testPool="nothing";
+  const addDice=()=>{
+    emit('addDice', {min:1, max:6, modifier:0});
+  };
 </script>
 
 <template>
-<div>
-  Dice Pool
-</div>
+  <div @click="addDice">Add Dice</div>
+  <div id="container">
+    {{ props.pool }}
+  </div>
 </template>
 
 <style scoped>
+  #container {
+    border: thin solid pink;
+  }
 </style>
