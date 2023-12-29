239f523a8795f0de0e91caaa1559960780370b2d
Snowb <ammoset@gmail.com>
2023-12-29 15:36:07 -0500

    clean up App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,9 @@
 <script setup>
-//import HelloWorld from './components/HelloWorld.vue'
-import { Dice } from './libs/dice-class-v1';
-import { Pool } from './libs/pool-class-v1';
 import PoolsContainer from './components/PoolsContainer.vue';
 
-const newPool=new Pool([new Dice(1,6),new Dice(1,6)]);
 </script>
 
 <template>
-  <!--div>{{newPool.getSets()}}</div>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />--->
   <div>
     <PoolsContainer></PoolsContainer>
   </div>
