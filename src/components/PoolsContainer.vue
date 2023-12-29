70796e245be20eaf1c02549184405ceb907df85e
Snowb <ammoset@gmail.com>
2023-12-29 13:47:43 -0500

    initial PoolsContainer component

---
+++ src/components/PoolsContainer.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+<div style="border-radius:0.2em; box-shadow:inset 0px 0px 5px 0px white; width: 100vw; display: flex; flex-direction: column; padding:0.2em;">
+  <div>
+    <span style="font-weight:bold; border-bottom:thin solid rgba(255,255,255,0.5); border-right:thin solid rgba(255,255,255,0.5); border-radius: 0.2em; padding:0em 0.5em 0em 0.5em;">
+      Pools Container
+    </span>
+  </div>
+  <div>
+    <span style="border:thin solid white; padding:0em 0.5em 0em 0.5em; border-radius: 0.2em;">Add Pool</span>
+  </div>
+</div>
+</template>
+
+<style scoped>
+</style>
