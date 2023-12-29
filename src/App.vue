5bfdf85a583d01a72923aea7f66331eeed207076
Snowb <ammoset@gmail.com>
2023-12-29 13:47:34 -0500

    initial setup of App.vue, index.html and style.css

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 import { Dice } from './libs/dice-class-v1';
 import { Pool } from './libs/pool-class-v1';
+import PoolsContainer from './components/PoolsContainer.vue';
 
 const newPool=new Pool([new Dice(1,6),new Dice(1,6)]);
 </script>
 
 <template>
-  <div>{{newPool.getSets()}}</div>
+  <!--div>{{newPool.getSets()}}</div>
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -16,20 +17,11 @@ const newPool=new Pool([new Dice(1,6),new Dice(1,6)]);
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />--->
+  <div>
+    <PoolsContainer></PoolsContainer>
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
