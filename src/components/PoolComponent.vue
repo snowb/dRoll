fd15b43431a4fdf0a3ad873e1ce13574b907637b
Snowb <ammoset@gmail.com>
2023-12-30 10:22:45 -0500

    getting vue flow working with Pool object

--- src/components/PoolComponent.vue
+++ src/components/PoolComponent.vue
@@ -1,24 +1,46 @@
 <script setup>
-  import { reactive, defineProps } from 'vue';
-  import { Pool } from '../libs/pool-class-v1';
+  import { defineProps, toRaw, watchEffect, ref, unref, watch, shallowReactive} from 'vue';
 
   const props=defineProps({
     pool: Object,
+    pool_index: Number
   });
 
   const emit=defineEmits(['addDice']);
-
-  const testPool="nothing";
   const addDice=()=>{
-    emit('addDice', {min:1, max:6, modifier:0});
+    emit('addDice', {pool_index:props.pool_index, min:1, max:6, modifier:0});
   };
+  const showPool=(_dice)=>{
+    console.log("showing",toRaw(_dice));
+    return "showing "+toRaw(_dice).getMinimum()+"D"+toRaw(_dice).getMaximum();
+  }
+
+  let rawPoolFullResults=null;
+  let refPoolStringFullResults=ref("test1");
+  let count=0;
+  watchEffect(()=>{
+    //let pool=props.pool;
+    console.log("watchPostEffect 1",props.pool)
+    console.log("watchPostEffect 2",unref(props.pool))
+    //let pool=props.pool.value;
+    //rawPoolFullResults=toRaw(pool); 
+    //rawPoolFullResults==pool; 
+    //console.log("PoolComponent : watchPostEffect",rawPoolFullResults);
+    refPoolStringFullResults.value="test"+count;//JSON.stringify(rawPoolFullResults);
+    count++;
+    });
+  watch(props.pool, (_pool, _old_pool)=>{
+    console.log("pool changed")
+  });
 </script>
 
 <template>
-  <div @click="addDice">Add Dice</div>
-  <div id="container">
-    {{ props.pool }}
-  </div>
+  <span>
+    <span style="border:thick solid red; border-radius: 0.2em; padding: 0em 0.5em 0em 0.5em" @click="addDice">Add Dice</span>
+    <div v-for="(dice,dice_index) in unref(props.pool)">{{ showPool(dice) }} at dice_idnex:{{ dice_index }}</div>
+    <div>{{ refPoolStringFullResults }}</div>
+  </span>
+  
 </template>
 
 <style scoped>
--- src/components/PoolsContainer.vue
+++ src/components/PoolsContainer.vue
@@ -1,15 +1,19 @@
 <script setup>
-  import { reactive } from 'vue';
+  import { reactive, ref, unref, shallowReactive, toRaw, triggerRef, shallowRef } from 'vue';
   import PoolComponent from './PoolComponent.vue';
   import { Pool } from '../libs/pool-class-v1';
   
-  const pools=reactive([]);
+  const pools=shallowRef([]);
 
   const addPool=()=>{
-    pools.push(new Pool());
+    pools.value=[...pools.value, new Pool()];
+    triggerRef(pools);
   };
   const addDice=(_arguments)=>{
-    console.log("addDice", _arguments)
+    console.log("PoolsContainer: addDice 1",toRaw(pools));
+    toRaw(pools.value[_arguments.pool_index]).addDice(_arguments.min, _arguments.max, _arguments.modifier);
+    console.log("PoolsContainer: addDice 2",pools[_arguments.pool_index])
+    triggerRef(pools);
   }
 </script>
 
@@ -20,10 +24,10 @@
       Pools Container
     </span>
   </div>
+  <PoolComponent v-for="(pool,pool_index) in pools" :pool=ref(pool.getFullRollResults()) :pool_index=pool_index @addDice="addDice"></PoolComponent>
   <div> 
     <span style="border:thin solid white; padding:0em 0.5em 0em 0.5em; border-radius: 0.2em;" @click="addPool()">Add Pool</span>
   </div>
-  <PoolComponent v-for="pool in pools" :pool=pool @addDice="addDice"></PoolComponent>
 </div>
 </template>
 
