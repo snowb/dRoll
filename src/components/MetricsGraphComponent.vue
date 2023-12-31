7ebe68747e7e36fa35fb2484228e7d53f24966e4
Snowb <ammoset@gmail.com>
2023-12-31 09:49:52 -0500

    begin MetricsGraph work

--- src/components/MetricsGraphComponent.vue
+++ src/components/MetricsGraphComponent.vue
@@ -1,9 +1,16 @@
 <script setup>
 //Component for displaying graph of data
+defineProps({
+  metrics: Array,
+  iterations: Number,
+  force_render: Number
+});
 </script>
 
 <template>
-
+  <div>
+    {{metrics}}
+  </div>
 </template>
 
 <style scoped>
--- src/components/PoolMetricsComponent.vue
+++ src/components/PoolMetricsComponent.vue
@@ -1,13 +1,15 @@
 <script setup>
 //Component for displaying metrics of associated Pool object
+import MetricsGraphComponent from './MetricsGraphComponent.vue';
+
   const props=defineProps({
-    metrics: Object,
+    metrics: Array,
     force_render: Number
   });
 </script>
 
 <template>
-  <div>{{ props.metrics }}</div>
+  <MetricsGraphComponent :metrics="props.metrics" :force_render="props.force_render"></MetricsGraphComponent>
 </template>
 
 <style scoped>
