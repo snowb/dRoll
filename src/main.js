f34206082bedb944bcbb17b79ec4c2fd6c270a6e
Snowb <ammoset@gmail.com>
2024-01-04 19:14:44 -0500

    commenting what icon is be used for what, and planning

--- src/main.js
+++ src/main.js
@@ -5,14 +5,25 @@ import { OhVueIcon, addIcons } from "oh-vue-icons";
 import { IoCloseCircle, 
   GiCrownedExplosion, 
   BiArrowRepeat, 
-  BiGearFill, 
+  BiGearFill,
+  BiDice6Fill,
+  GiCardBurn,
   FaDiceD20 } from "oh-vue-icons/icons";
 
 addIcons(IoCloseCircle, 
-  GiCrownedExplosion, 
-  BiArrowRepeat, 
-  BiGearFill, 
-  FaDiceD20);
+  GiCrownedExplosion, //for exploding dice 
+  BiArrowRepeat,      //for re-roll 
+  BiGearFill,         //for setting change ... switch to   FaExchangeAlt?
+  BiDice6Fill,        //for Add Dice (maybe)
+  GiCardBurn,         //for Add Deck (maybe, need to modify Path)
+                      // combined icon for show/hide Sets?
+                      // combined icon for show/hide Sequences?
+                      // {FcCancel | FaSlash | FaSlashSolid | BiSlashLg | BiSlash} to combine with Sets/Sequences ... other?
+                      // HiSolidFilter for filtering
+                      // Below X - BiArrowBarDown, FaLessThan
+                      // Above X - BiArrowBarUp, FaGreaterThan
+                      // Equal to X - FaEquals
+  FaDiceD20);         //for D20 ... Add Dice?
 
 const app=createApp(App);
 app.component("v-icon", OhVueIcon);
