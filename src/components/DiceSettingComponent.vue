32fac51999e788e096af1bdebd526107f6a7c188
Snowb <ammoset@gmail.com>
2024-01-06 16:37:18 -0500

    initial create of DiceSettingComponent

---
+++ src/components/DiceSettingComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { isNumeric } from '../libs/isNumeric';
+</script>
+
+<template>
+  <div>
+    This is the dice settings component
+  </div>
+</template>
+
+<style scoped>
+
+</style>
