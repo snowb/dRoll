4212cb8f945d40d2d6a162a8a45d7d74e0c165b5
Snowb <ammoset@gmail.com>
2024-01-07 10:51:38 -0500

    combine modifying functions, implement remaining explode options

--- src/components/DiceSettingComponent.vue
+++ src/components/DiceSettingComponent.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { Dice } from '../libs/dice-class';
 import { isNumeric } from '../libs/isNumeric';
-import { ref, toRaw, computed, watchPostEffect, watchEffect } from 'vue';
+import { ref, reactive, toRaw, computed, watchEffect } from 'vue';
 
 const props=defineProps({
     dice: Dice,
@@ -9,51 +9,72 @@ const props=defineProps({
     force_render: Number
   });
 
-const emit=defineEmits(["explode"])
+const emit=defineEmits(["explode"]);
+
+let explode_options=reactive({});
+explode_options.target_explode_on="max";
+explode_options.explode_on_value=props.dice.getMaximum();
+explode_options.added_dice_minimum=1;
+explode_options.added_dice_maximum=props.dice.getMaximum();
+explode_options.explosion_limit=1;
 
 let showExplodeOptions=ref(false);
 // let showFilterOptions=ref(false);
 // let showDropOptions=ref(false);
 
-let target_explode_on="max";
 
 const updateExplode=(_event)=>{
-  target_explode_on=_event.target.value;
+  explode_options.target_explode_on=_event.target.value;
   if(explode_emitted.value){
-    emit("explode", target_explode_on, explode_on_value.value);
-  }
-};
-
-let explode_on_value=ref(null);
-const editValue=(_event)=>{
-  if(_event.key=="Enter"){
-    _event.preventDefault();
-  }
-  if(!isNumeric(_event.target.innerText)){
-    _event.target.innerText=explode_on_value.value;
-    return undefined;
-  }
-  explode_on_value.value=+_event.target.innerText;
-  if(explode_emitted.value){
-    emit("explode", target_explode_on, explode_on_value.value);
+    emit("explode", explode_options);
   }
 };
 
 let explode_emitted=ref(false);
 const emitExplode=()=>{
-  emit("explode",target_explode_on,explode_on_value.value);
+  emit("explode",explode_options);
   explode_emitted.value=true;
 };
 
-watchEffect(()=>{
-  props.dice;
-  props.force_render;
-  if(props.dice===undefined){
-    explode_on_value.value="";
-  } else if(explode_on_value.value===null){
-    explode_on_value.value=toRaw(props.dice).getMaximum();
+const updateExplodeOption=(_event, _target_option)=>{
+  if(_event.key=="Enter"){
+    _event.preventDefault();
   }
-});
+  if(!isNumeric(_event.target.innerText)){
+    switch(_target_option){
+      case "on_value":
+        _event.target.innerText = explode_options.explode_on_value;
+        break;
+      case "add_max":
+        _event.target.innerText = explode_options.added_dice_maximum;
+        break;
+      case "add_min":
+        _event.target.innerText = explode_options.added_dice_minimum;
+        break;
+      case "limit":
+        _event.target.innerText = explode_options.explosion_limit;
+        break;
+    }
+    return undefined;
+  }
+  switch(_target_option){
+    case "on_value":
+      explode_options.explode_on_value = +_event.target.innerText;
+      break;
+    case "add_max":
+      explode_options.added_dice_maximum = +_event.target.innerText;
+      break;
+    case "add_min":
+      explode_options.added_dice_minimum = +_event.target.innerText;
+      break;
+    case "limit":
+      explode_options.explosion_limit = +_event.target.innerText;
+      break;
+  }
+  if(explode_emitted.value){
+    emit("explode", explode_options);
+  }
+};
 </script>
 
 <template>
@@ -62,18 +83,18 @@ watchEffect(()=>{
     <div v-if="showExplodeOptions" class="small" style="padding-left:0.5em;">
       <span><input type="radio" name="explode_on" value="max" :checked="true" @change="updateExplode"/>On Max</span>&nbsp;&nbsp;
       <span><input type="radio" name="explode_on"  value="min" :checked="false" @change="updateExplode"/>On Min</span> &nbsp;&nbsp;
-      <span><input type="radio" name="explode_on"  value="value" :checked="false" @change="updateExplode"/>On Value: <span class="editable" contenteditable @keydown.enter="editValue" @blur="editValue">{{ explode_on_value }}</span></span>
+      <span><input type="radio" name="explode_on"  value="value" :checked="false" @change="updateExplode"/>On Value: <span class="editable" contenteditable @keydown.enter="updateExplodeOption($event, 'on_value')" @blur="updateExplodeOption($event, 'on_value')">{{ explode_options.explode_on_value }}</span></span>
       <br><br>
       <span style="display: flex; flex-direction: row;">
         <span>
           &nbsp;&nbsp;Added Dice
           <br>
           <!-- IMPLEMENT THIS SECTION -->
-          <span><input type="radio" name="explode_dice" :checked="true"/>d<span class="editable" contenteditable>X</span></span>&nbsp;&nbsp;
-          <span><input type="radio" name="explode_dice" :checked="false"/><span class="editable" contenteditable>X</span> to <span class="editable" contenteditable>Y</span></span>
+          <span><input type="radio" name="explode_dice" :checked="true"/>d<span class="editable" @keydown.enter="updateExplodeOption($event, 'add_max')" @blur="updateExplodeOption($event, 'add_max')" contenteditable>{{ explode_options.added_dice_maximum }}</span></span>&nbsp;&nbsp;
+          <span><input type="radio" name="explode_dice" :checked="false"/><span class="editable" @keydown.enter="updateExplodeOption($event, 'add_min')" @blur="updateExplodeOption($event, 'add_min')" contenteditable>{{ explode_options.added_dice_minimum }}</span> to <span class="editable" @keydown.enter="updateExplodeOption($event, 'add_max')" @blur="updateExplodeOption($event, 'add_max')" contenteditable>{{ explode_options.added_dice_maximum }}</span></span>
         </span>
         <span>
-          &nbsp;&nbsp;&nbsp;&nbsp;Explosion Limit: <span class="editable" contenteditable>1</span>
+          &nbsp;&nbsp;&nbsp;&nbsp;Explosion Limit: <span @keydown.enter="updateExplodeOption($event, 'limit')" @blur="updateExplodeOption($event, 'limit')" class="editable" contenteditable>{{ explode_options.explosion_limit }}</span>
         </span>
       </span>
       <div style="display: flex; flex-direction: row; position: relative; margin-bottom:0.2em;">
